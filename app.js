e84ebbefba1cefb0a3aabbcc7d442a01f72205b4
GAUTAM DUTTA <gautam.dutta@in.ibm.com>
2018-01-29 13:27:09 +0530

    code update

--- app.js
+++ app.js
@@ -7,7 +7,7 @@
 // This application uses express as its web server
 // for more info, see: http://expressjs.com
 var express = require('express');
-var multer = require('multer');
+
 
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
