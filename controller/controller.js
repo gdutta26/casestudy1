984b52c98bb8d267fec87eb94fbbe2accf27b8e4
GAUTAM DUTTA <gautam.dutta@in.ibm.com>
2018-01-31 14:23:56 +0530

    update the change

--- controller/controller.js
+++ controller/controller.js
@@ -24,11 +24,12 @@ module.exports = function(app) {
                 res.json(found);    
                 res.end();
             });            
-    });
+    });/* 
     
     //router.post('/emailnotificationCron/attachmentUpload', multer({
     	 app.post('/emailnotificationCron/attachmentUpload', multer({
-        dest: path.join(__dirname, '../attachment'),
+        //dest: path.join(__dirname, '../attachment'),
+        dest: __dirname+ '/attachment',
         rename: function (fieldname, filename) {
             return filename;
         },
@@ -57,6 +58,37 @@ module.exports = function(app) {
         },
         inMemory: true
     }),function(request1, response1) {
-    });
+    }); */
     
+	app.post('/emailnotificationCron/attachmentUpload', multer({
+		dest: __dirname+ '/attachment',
+        rename: function (fieldname, filename) {
+            return filename;
+        },
+        onFileUploadStart: function (file) {
+        },
+        onFileUploadData: function (file, data, req, res) {
+        },
+        onFileUploadComplete: function (file, req, res) {
+            var response = {};
+            var userid = req.body.userid;
+            var buff = new Buffer(file.buffer, 'base64');
+            getDir().then(function(filePath){
+                fs.writeFile(filePath+"/"+file.name, buff, function(err){
+                    if (err) throw err
+                    fetchUserDoc(userid).then(function(userDocData){
+                        var rev = userDocData["rev"];
+                        var docId = "cat_user_"+userid.toUpperCase();
+                        saveAttachmentToDB(docId,filePath,file.name,rev).then(function(responseSave){
+                            response.totalfilepath = config1["cloudantURIDB"]+"/"+docId+"/"+file.name;
+                            response.filename = file.name;
+                            res.status(200).send(response);
+                        });
+                    });
+                });
+            });
+        },
+        inMemory: true
+	}).single("test"),function(request1, response1) {
+    })
 }
