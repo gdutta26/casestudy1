9713825cb825dd78db553357bdf0eeb9a5da45a1
GAUTAM DUTTA <gautam.dutta@in.ibm.com>
2018-02-01 16:54:00 +0530

    change the script

--- controller/controller.js
+++ controller/controller.js
@@ -25,43 +25,7 @@ module.exports = function(app) {
                 res.end();
             });            
     });
-    
-    /* 
-    
-    //router.post('/emailnotificationCron/attachmentUpload', multer({
-    	 app.post('/emailnotificationCron/attachmentUpload', multer({
-        //dest: path.join(__dirname, '../attachment'),
-        dest: __dirname+ '/attachment',
-        rename: function (fieldname, filename) {
-            return filename;
-        },
-        onFileUploadStart: function (file) {
-        },
-        onFileUploadData: function (file, data, req, res) {
-        },
-        onFileUploadComplete: function (file, req, res) {
-            var response = {};
-            var userid = req.body.userid;
-            var buff = new Buffer(file.buffer, 'base64');
-            getDir().then(function(filePath){
-                fs.writeFile(filePath+"/"+file.name, buff, function(err){
-                    if (err) throw err
-                    fetchUserDoc(userid).then(function(userDocData){
-                        var rev = userDocData["rev"];
-                        var docId = "cat_user_"+userid.toUpperCase();
-                        saveAttachmentToDB(docId,filePath,file.name,rev).then(function(responseSave){
-                            response.totalfilepath = config1["cloudantURIDB"]+"/"+docId+"/"+file.name;
-                            response.filename = file.name;
-                            res.status(200).send(response);
-                        });
-                    });
-                });
-            });
-        },
-        inMemory: true
-    }),function(request1, response1) {
-    }); */
-	
+    	
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: false }));
 	app.post('/emailnotificationCron/attachmentUpload', multer({
