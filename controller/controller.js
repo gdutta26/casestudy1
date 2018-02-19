affadbb5f1b3115892a2433c67e4cf774b26b74f
Angshuman Dey <angsdey2@in.ibm.com>
2018-02-20 00:35:14 +0530

    File upload and File View added

--- controller/controller.js
+++ controller/controller.js
@@ -1,7 +1,9 @@
 var services = require("../service/node_cloudant_service");
 var bodyParser  = require("body-parser");
 var multer = require("multer");
-
+var request = require('request');
+var path = require('path');
+var upload = multer({ dest: 'uploads/' })
 
 
 // all service Request comes here
@@ -35,59 +37,46 @@ module.exports = function(app) {
         });
     });
 
-
-    var storage = multer.diskStorage({
-        destination: function(req, file, callback) {
-            callback(null, './uploads')
-        },
-        filename: function(req, file, callback) {
-            console.log(file)
-            callback(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
-        }
+    app.post('/api/uploadFile', upload.single('upload'), function (req, res, next) {
+		
+		console.log(req.file);
+		services.uploadFile(res, req.file , req.body, function (found) {
+			res.json(found);    
+			res.end();
+		}); 
+		
+    }); 
+	
+	app.post('/api/uploadFile', upload.single('upload'), function (req, res, next) {
+		
+		console.log(req.file);
+		services.uploadFile(res, req.file , req.body, function (found) {
+			res.json(found);    
+			res.end();
+		}); 
+		
     });
-    var upload = multer({storage: storage}).single("upload")
-
-    app.use(bodyParser.json());
+	
+	app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: false }));
-    app.post('/', function(req , res){
-        upload(req,res, function (err){
-
-            console.log("Within upload");
-            console.log(req);
-        });
+    app.post('/api/searchFiles', function(req , res){
+           var details = req.body;
+            services.searchFiles(res, details , function (found) {
+                res.json(found);    
+                res.end();
+            });            
+    });
+	
+	app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.post('/api/downloadFiles', function(req , res){
+			console.log("Within Controller");
+			console.log(req.body);
+           var details = req.body;
+            services.downloadFiles(res, details , function (found) {
+                res.json(found);    
+                res.end();
+            });            
     });
 
-
-
-
-
-
-
-   /* app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({ extended: false }));
-	app.post('/emailnotificationCron/attachmentUpload', multer({
-		dest: __dirname+ '/attachment',
-        rename: function (fieldname, filename) {
-            return filename;
-        },
-     inMemory: true
-
-	}).single("upload"),function(request1, response1) {
-
-		var newfile = new NewFile({
-			fieldname: request1.file.fieldname,
-		    originalname: request1.file.originalname,
-		    destination:request1.file.destination,
-		    filename: request1.file.filename,
-		    path: request1.file.path,
-		    size: request1.file.size
-
-		})
-		 newfile.save(function(err){
-			    if (err){console.log(err)}
-			    else {
-			      res.redirect('/');
-			    }
-	    })
-});*/
 }
